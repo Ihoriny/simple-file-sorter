1567475478e133104e61b3a3a4af71cc8c5f649b
Ihor Berehovskyi <regionbereg@gmail.com>
2024-11-30 22:15:29 +0200

    v1.2 Problems fix, implemented new feature
    - Fixed bug with spaces in filenames
    - Added sorting files without extensions to separate folder

--- sort.sh
+++ sort.sh
@@ -9,6 +9,11 @@ fi
 
 DIR_PATH="${1%/}/"
 
+if [ ! -d "$DIR_PATH" ]; then
+    echo "$DIR_PATH is not a directory."
+    exit 1
+fi
+
 if [ ! -w "$DIR_PATH" ]; then
     echo "No write permissions for $DIR_PATH"
     exit 1
@@ -16,12 +21,21 @@ fi
 
 for file in "$DIR_PATH"*; do
     
-    if [ -f "$file" ] && [ ! -L "$file" ]; then
-        FILES_DIR_PATH="$DIR_PATH${file##*.}"
-
-        mkdir -p $FILES_DIR_PATH
-        mv -n $file $FILES_DIR_PATH
+    if [[ ! -f "$file" || -L "$file" ]]; then
+        continue
     fi
+
+    EXT="${file##*.}"
+
+    if [ -z "$EXT" ]; then
+        FILES_DIR_PATH="${DIR_PATH}no_extension"
+    else
+        FILES_DIR_PATH="$DIR_PATH$EXT"
+    fi
+
+    mkdir -p "$FILES_DIR_PATH"
+    mv -n "$file" "$FILES_DIR_PATH"
+
 done
 
 exit 0
